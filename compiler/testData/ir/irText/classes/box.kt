203461b7c5a4f8a86c7bdb4462425124734f27bd
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-02-26 15:56:21 +0300

    FIR2IR: in case of use-site generic type use call from original class

--- compiler/testData/ir/irText/classes/box.kt
+++ compiler/testData/ir/irText/classes/box.kt
@@ -1,8 +1,15 @@
-class Box<T>(t: T) {
+open class Box<T>(t: T) {
     var value = t
 }
 
+class DerivedBox : Box<Int>(1)
+
 fun box(): String {
     val box: Box<Int> = Box<Int>(1)
+    return if (box.value == 1) "OK" else "fail"
+}
+
+fun derivedBox(): String {
+    val box: DerivedBox = DerivedBox()
     return if (box.value == 1) "OK" else "fail"
 }
